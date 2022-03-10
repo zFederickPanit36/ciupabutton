a1113069d9b69f5acd394ce9f58121adb5efacb0
zFederickPanit36 <86781813+zFederickPanit36@users.noreply.github.com>
2022-03-10 23:32:05 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
 let ifPressedOneTime = false;
 let counter = 0;
 
+const counterBox = document.getElementById("counter");
+
+const doCiupa = (counter) => {
+    counter++;
+    counterBox.innerHTML = `Ciupa counter: <span id="color">${counter}</span>`;
+    console.log(`ciupa counter: ${counter}`);
+    return counter;
+}
+
 const ciupa = document.getElementById("ciupaButton").onclick = () => {
     let button = document.getElementById("ciupaButton");
     let h1 = document.getElementById("sas");
-    let counterBox = document.getElementById("counter");
-    let msg = `Ciupa counter: <span id="color">${counter}</span>`;
 
     button.innerHTML = "eh";
 
@@ -13,24 +20,18 @@ const ciupa = document.getElementById("ciupaButton").onclick = () => {
         switch (h1.innerHTML) {
             case "CLICCA IL BOTTONE":
                 h1.innerHTML = "CIUPA";
-                counter++;
-                counterBox.innerHTML = msg;
-                console.log("ciupa");
+                counter = doCiupa(counter);
                 break;
             case "CIUPA":
                 h1.innerHTML = "EH";
                 button.innerHTML = "ciupa";
-
+                counter = doCiupa(counter);
                 break;
             case "EH":
                 h1.innerHTML = "CLICCA IL BOTTONE";
                 button.innerHTML = "click me";
                 break;
         }
-    }
-
-    if (counter === 69) {
-        alert("sex");
     }
 
     (button.innerHTML === "click me") ? ifPressedOneTime = false: ifPressedOneTime = true;
