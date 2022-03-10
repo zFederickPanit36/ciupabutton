5c6ce0b3755a82315a4a88bee7f6316d03ecff74
zFederickPanit36 <86781813+zFederickPanit36@users.noreply.github.com>
2022-03-10 23:03:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 let ifPressedOneTime = false;
+let counter = 0;
 
 const ciupa = document.getElementById("ciupaButton").onclick = () => {
     let button = document.getElementById("ciupaButton");
     let h1 = document.getElementById("sas");
+    let counterBox = document.getElementById("counter");
+    let msg = `Ciupa counter: <span id="color">${counter}</span>`;
 
     button.innerHTML = "eh";
 
@@ -10,16 +13,24 @@ const ciupa = document.getElementById("ciupaButton").onclick = () => {
         switch (h1.innerHTML) {
             case "CLICCA IL BOTTONE":
                 h1.innerHTML = "CIUPA";
+                counter++;
+                counterBox.innerHTML = msg;
+                console.log("ciupa");
                 break;
             case "CIUPA":
                 h1.innerHTML = "EH";
                 button.innerHTML = "ciupa";
+
                 break;
             case "EH":
                 h1.innerHTML = "CLICCA IL BOTTONE";
                 button.innerHTML = "click me";
                 break;
         }
+    }
+
+    if (counter === 69) {
+        alert("sex");
     }
 
     (button.innerHTML === "click me") ? ifPressedOneTime = false: ifPressedOneTime = true;
